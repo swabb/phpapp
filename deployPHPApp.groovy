8e04786282e0f44a7cf6baec4235c998916e7fc0
Vladimir Sadkov <swab@ukr.net>
2020-03-17 16:31:27 +0200

    Fix typo

--- deployPHPApp.groovy
+++ deployPHPApp.groovy
@@ -1,10 +1,10 @@
-node(labels: 'master'){
+node(label: 'master'){
     stage('Get repo'){
         println('Stage: ')
         sh "rm -rf ${WORKSPACE}/*"
         sh "mkdir ${WORKSPACE}/build"
         dir('build'){
-            git branch: "${REPO_BRANCH}", url: "git@github.com:swabb/phpapp.git"
+            git branch: "${REPO_BRANCH}", url: "https://github.com/swabb/phpapp.git"
         }
     }
 }
