663a03f9426bc85e7cbbb0b5ec89a62056f78439
Vladimir Sadkov <swab@ukr.net>
2020-03-18 10:59:29 +0200

    Fix redeploy

--- deployPHPApp.groovy
+++ deployPHPApp.groovy
@@ -13,19 +13,20 @@ node(label: 'master'){
 
                 sh "docker build -t 217993088031.dkr.ecr.eu-west-2.amazonaws.com/vsadkov-phpapp-nginx:latest -f ./Dockerfile.nginx ."
                 sh "docker push 217993088031.dkr.ecr.eu-west-2.amazonaws.com/vsadkov-phpapp-nginx:latest"
-                sh "docker rmi 217993088031.dkr.ecr.eu-west-2.amazonaws.com/vsadkov-phpapp-nginx:latest"
+                // sh "docker rmi 217993088031.dkr.ecr.eu-west-2.amazonaws.com/vsadkov-phpapp-nginx:latest"
                 
                 sh "docker build -t 217993088031.dkr.ecr.eu-west-2.amazonaws.com/vsadkov-phpapp-phpfpm:latest -f ./Dockerfile.phpfpm ."
                 sh "docker push 217993088031.dkr.ecr.eu-west-2.amazonaws.com/vsadkov-phpapp-phpfpm:latest"
-                sh "docker rmi 217993088031.dkr.ecr.eu-west-2.amazonaws.com/vsadkov-phpapp-phpfpm:latest"
+                // sh "docker rmi 217993088031.dkr.ecr.eu-west-2.amazonaws.com/vsadkov-phpapp-phpfpm:latest"
 
                 if (REDEPLOY == "true") {
                     println('== Redeploy application. Delete old deploy.')
                     sh "kubectl delete -f phpapp-env.yaml"
                     sh "kubectl delete -f phpapp-deployment.yaml"
+                } else {
+                    sh "kubectl create -f phpapp-cert.yaml"
                 }
                 sh "kubectl create -f phpapp-env.yaml"
-                sh "kubectl create -f phpapp-cert.yaml"
                 sh "kubectl create -f phpapp-deployment.yaml"
                 
                 loadBalancer = sh returnStdout: true, script: "kubectl get svc -A | grep 'LoadBalancer' | awk '{print \$5}'"
